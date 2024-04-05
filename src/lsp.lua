8acdba8d155b80123eda4a16d8df07b0c480a004
Niels de Koeijer <nemk@bang-olufsen.dk>
2024-04-05 09:33:34 +0200

    lsp

--- src/lsp.lua
+++ src/lsp.lua
@@ -2,8 +2,9 @@
 dofile(Config_path .. '/src/' .. 'lsp/clangd.lua')
 dofile(Config_path .. '/src/' .. 'lsp/lua_ls.lua')
 dofile(Config_path .. '/src/' .. 'lsp/rustanalyzer_ls.lua')
+dofile(Config_path .. '/src/' .. 'lsp/zig_ls.lua')
 dofile(Config_path .. '/src/' .. 'lsp/python_ls.lua')
 
 -- always format on save
-vim.lsp.buf.format({ async = false })
-vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
+-- vim.lsp.buf.format({ async = false })
+-- vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
---
+++ src/lsp/zig_ls.lua
@@ -0,0 +1,30 @@
+-- lsp configuration for clang
+local autocmd = vim.api.nvim_create_autocmd
+autocmd("FileType", {
+    pattern = "zig",
+    callback = function()
+        local search = vim.fs.find({ '.git' }, { upward = true })
+        local root_dir = vim.fs.dirname(search[1])
+        Log("found root-dir: '" .. root_dir .. "'")
+
+        local custom_config_path = vim.fs.find({ '.lsp.lua' }, { path = root_dir })[1]
+        local config
+
+        if custom_config_path ~= nil then
+            Log("found custom config at '" .. custom_config_path .. "'")
+            package.path = package.path .. ";" .. custom_config_path
+            local custom_config = require('.lsp.lua')
+            config = custom_config.Get(root_dir)
+        else
+            Log("using default config")
+            config = {
+                name = 'zls',
+                cmd = {'zls'},
+                root_dir = root_dir,
+            }
+        end
+
+        local client = vim.lsp.start(config)
+        vim.lsp.buf_attach_client(0, client)
+    end
+})
